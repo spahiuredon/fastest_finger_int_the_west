ae77f1eebb21b8dbbb9182a40363e96aa50c5863
spahiuredon <redon.spahiu@stud.kbw.ch>
2021-07-08 16:35:16 +0200

    finished functionality of share button and touchable area

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -1,11 +1,92 @@
 import * as React from "react";
-import {Text, View} from "react-native";
+import {Share, StyleSheet, Text, TouchableOpacity} from "react-native";
+import {Button, NativeBaseProvider} from "native-base";
+import {useState} from "react";
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingBottom: -100
+    },
+
+    gametitle: {
+        fontFamily: "RobotoSlab-Regular",
+        fontSize: 27,
+        color: "#707070",
+        padding: 21,
+        paddingTop: 30,
+        paddingBottom: 0,
+        textAlign: 'center'
+
+    },
+
+    button: {
+        backgroundColor: '#D5884C',
+        width: '40.6%',
+        height: '7.2%',
+        alignSelf: 'center'
+    },
+
+    touch: {
+        marginTop: 30,
+        marginBottom: 0,
+        marginLeft: 20,
+        marginRight: 20,
+        borderRadius: 15,
+        borderColor: '#D5884C',
+        flex: 1,
+        borderWidth: 4,
+        borderStyle: 'solid',
+
+    },
+
+    score: {
+        fontFamily: "RobotoSlab-Regular",
+        fontSize: 30,
+        color: "#707070",
+        marginLeft: 20,
+        marginBottom: 100,
+        marginTop: 20,
+        textAlign: 'left'
+    }
+});
 
 function GameScreen() {
+    const [count, setCount] = useState(0);
+    const onPress = () => setCount(prevCount => prevCount + 1);
+
+    const onShare = async () => {
+        try {
+            const result = await Share.share({
+                message:
+                    "Hello "+count,
+            });
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                } else {
+                }
+            } else if (result.action === Share.dismissedAction) {
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
     return (
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text>Shake to start!</Text>
-        </View>
+        <NativeBaseProvider style={styles.screen}>
+                <Text style={styles.gametitle}>Shake to start!</Text>
+                <Text style={styles.gametitle}>Time</Text>
+            <TouchableOpacity
+                activeOpacity={1}
+                style={styles.touch}
+                onPress={onPress}
+            >
+            </TouchableOpacity>
+                <Text style={styles.score}>{count}</Text>
+            <Button onPress={()=>onShare()} title="Share" />
+        </NativeBaseProvider>
     );
 }
 
